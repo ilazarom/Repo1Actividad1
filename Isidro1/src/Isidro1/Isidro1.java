29c50117dd48a10045f4767a814260fc4f29689d
isidro.lazaro.masero <ilazarom17@gmail.com>
2023-02-06 17:18:39 +0100

    tres hobbies

--- Isidro1/src/Isidro1/Isidro1.java
+++ Isidro1/src/Isidro1/Isidro1.java
@@ -4,6 +4,10 @@ public class Isidro1 {
 
 	public static void main(String[] args) {
 		System.out.println("Soy Isidro1");
+		System.out.println("HOBBIES");
+		System.out.println("Fútbol");
+		System.out.println("Baloncesto");
+		System.out.println("Pádel");
 
 	}
 
