120e9f0de842c6f6439629d1ee76f0168011b650
isidro.lazaro.masero <ilazarom17@gmail.com>
2023-02-06 17:28:58 +0100

    tres Aficiones

--- Isidro2/src/Isidro2/Isidro2.java
+++ Isidro2/src/Isidro2/Isidro2.java
@@ -4,6 +4,10 @@ public class Isidro2 {
 
 	public static void main(String[] args) {
 		System.out.println("Soy Isidro2");
+		System.out.println("HOBBIES");
+		System.out.println("Teatro");
+		System.out.println("Cine");
+		System.out.println("Piraguismo");
 
 	}
 
