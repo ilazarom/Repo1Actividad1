8900d504cfe2e7b4a60f238da5bf44d5605727bd
isidro.lazaro.masero <ilazarom17@gmail.com>
2023-02-06 18:09:55 +0100

    comidas favoritas

--- Isidro2/src/Isidro2/Isidro2.java
+++ Isidro2/src/Isidro2/Isidro2.java
@@ -4,10 +4,10 @@ public class Isidro2 {
 
 	public static void main(String[] args) {
 		System.out.println("Soy Isidro2");
-		System.out.println("HOBBIES");
-		System.out.println("Teatro");
-		System.out.println("Cine");
-		System.out.println("Piraguismo");
+		System.out.println("COMIDAS");
+		System.out.println("Pizza");
+		System.out.println("Patatas");
+		System.out.println("Pasta");
 
 	}
 
